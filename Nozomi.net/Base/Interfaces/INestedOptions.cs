ea52fc0b766e75e25d15d4ee4032d9bdb733b23b
nixholas <nixholas@icloud.com>
2020-06-24 15:27:20 +0800

    Nozomi.net: Base: Interfaces: INestedOptions: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.net/Base/Interfaces/INestedOptions.cs
@@ -0,0 +1,8 @@
+namespace Nozomi.net.Base.Interfaces
+{
+    public interface INestedOptions
+    {
+        // this interface just needs to be implemented by any class that is
+        // a nested object under any of the primary service options
+    }
+}
