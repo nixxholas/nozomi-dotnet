b48f430e927af7010c8320a6e5a8d29f67b81256
nixholas <nixholas@icloud.com>
2020-06-24 13:33:51 +0800

    Nozomi.net: Infrastructure: Public: NozomiResponse: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.net/Infrastructure/Public/NozomiResponse.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Nozomi.net.Infrastructure.Public
+{
+    /// <summary>
+    /// Represents a response from Nozomi's API.
+    /// </summary>
+    public class NozomiResponse
+    {
+        /// <summary>Initializes a new instance of the <see cref="NozomiResponse"/> class.</summary>
+        /// <param name="statusCode">The HTTP status code.</param>
+        /// <param name="headers">The HTTP headers of the response.</param>
+        /// <param name="content">The body of the response.</param>
+        public NozomiResponse(HttpStatusCode statusCode, HttpResponseHeaders headers, string content)
+        {
+            this.StatusCode = statusCode;
+            this.Headers = headers;
+            this.Content = content;
+        }
+
+        /// <summary>Gets the HTTP status code of the response.</summary>
+        /// <value>The HTTP status code of the response.</value>
+        public HttpStatusCode StatusCode { get; }
+
+        /// <summary>Gets the HTTP headers of the response.</summary>
+        /// <value>The HTTP headers of the response.</value>
+        public HttpResponseHeaders Headers { get; }
+
+        /// <summary>Gets the body of the response.</summary>
+        /// <value>The body of the response.</value>
+        public string Content { get; }
+
+        /// <summary>Gets the date of the request, as returned by Nozomi.</summary>
+        /// <value>The date of the request, as returned by Nozomi.</value>
+        public DateTimeOffset? Date => this.Headers?.Date;
+
+        /// <summary>Gets the idempotency key of the request, as returned by Nozomi.</summary>
+        /// <value>The idempotency key of the request, as returned by Nozomi.</value>
+        public string IdempotencyKey => MaybeGetHeader(this.Headers, "Idempotency-Key");
+
+        /// <summary>Gets the ID of the request, as returned by Stripe.</summary>
+        /// <value>The ID of the request, as returned by Stripe.</value>
+        public string RequestId => MaybeGetHeader(this.Headers, "Request-Id");
+
+        internal int NumRetries { get; set; }
+
+        /// <summary>Returns a string that represents the <see cref="NozomiResponse"/>.</summary>
+        /// <returns>A string that represents the <see cref="NozomiResponse"/>.</returns>
+        public override string ToString()
+        {
+            return string.Format(
+                "<{0} status={1} Request-Id={2} Date={3}>",
+                this.GetType().FullName,
+                (int)this.StatusCode,
+                this.RequestId,
+                this.Date?.ToString("s"));
+        }
+
+        private static string MaybeGetHeader(HttpHeaders headers, string name)
+        {
+            if ((headers == null) || (!headers.Contains(name)))
+            {
+                return null;
+            }
+
+            return headers.GetValues(name).First();
+        }
+    }
+}
