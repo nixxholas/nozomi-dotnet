f640a993ec5fd836ddd4d2439e2e8ca7c5ab9cfd
nixholas <nixholas@icloud.com>
2020-07-10 12:43:17 +0800

    Nozomi.net: NozomiHttpClient: Propagate all constructors
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Nozomi.net/NozomiHttpClient.cs
+++ Nozomi.net/NozomiHttpClient.cs
@@ -9,6 +9,22 @@ namespace Nozomi.net
         
         public NozomiHttpClient(string apiKey)
         {
+            SetApiKey(apiKey);
+        }
+
+        public NozomiHttpClient(string apiKey, HttpMessageHandler handler) : base(handler)
+        {
+            SetApiKey(apiKey);
+        }
+
+        public NozomiHttpClient(string apiKey, HttpMessageHandler handler, bool disposeHandler) 
+            : base(handler, disposeHandler)
+        {
+            SetApiKey(apiKey);
+        }
+
+        private void SetApiKey(string apiKey)
+        {
             if (string.IsNullOrEmpty(apiKey))
                 throw new NullReferenceException("Invalid API Key");
             ApiKey = apiKey;
