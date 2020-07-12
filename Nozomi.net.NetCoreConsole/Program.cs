aeb67d20b99d768a3848c37536650497112c8762
nixholas <nixholas@icloud.com>
2020-07-12 18:43:25 +0800

    Nozomi.net.NetCoreConsole: Initial implementation
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Nozomi.net.NetCoreConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Nozomi.net.NetCoreConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var envVars = Environment.GetEnvironmentVariables();
+            if (envVars["key"] == null && string.IsNullOrEmpty((string) envVars["key"]))
+                throw new KeyNotFoundException("Please provide an API Key.");
+            
+            var apiClient = new NozomiApiClient((string) envVars["key"]);
+
+            var result = await apiClient.ConnectService.HEADWithHttpMessagesAsync();
+        }
+    }
+}
